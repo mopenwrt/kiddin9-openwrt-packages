2795f1de4ee5decc7023fe3843bcb9fb2a18a1e3
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-08-26 23:38:24 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -8,19 +8,18 @@
 po_file="$({ find luci-app* -type f -maxdepth 3 -name "*.po"; } 2>"/dev/null")"
 for a in ${po_file}
 do
-	[ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
+	sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
 done
 
 po_dir="$({ find luci-app* -type d -maxdepth 2 -name "zh-cn"; } 2>"/dev/null")"
 for e in ${po_dir}
 do
-	po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
-	mv "$e" "${po_new_dir}" 2>"/dev/null"
+	ln -sf zh_Hans $e
 done
 
 makefile_file="$({ find luci-app* -type f -maxdepth 1 -name Makefile; } 2>"/dev/null")"
 for f in ${makefile_file}
 do
-	[ -n "$(grep "zh-cn/" "$f")" ] && sed -i "s/zh-cn\//zh_Hans\//g" "$f"
-	[ -n "$(grep "zh_Hans.lmo" "$f")" ] && sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$fn"
+	sed -i "s/zh-cn\//zh_Hans\//g" "$f"
+	sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$f"
 done
