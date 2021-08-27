1295173b848050033d9df045903c42a81b32c42a
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-08-28 00:08:38 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# [CTCGFW]Project-OpenWrt
-# Use it under GPLv3, please.
-# --------------------------------------------------------
-# Convert translation files zh-cn to zh_Hans
-# The script is still in testing, welcome to report bugs.
 
 po_dir="$({ find luci-app* -type d -maxdepth 2 -name "zh-cn"; } 2>"/dev/null")"
 for e in ${po_dir}
