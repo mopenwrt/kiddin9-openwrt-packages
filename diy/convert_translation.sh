6846b8914e55c13af629ea6f60e5ab69860ac329
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-09-02 01:35:48 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for pkg in $(ls -d luci-*); do
-	ln -s zh-cn $ipk/po/zh_Hans 2>/dev/null
-	ln -s zh_Hans $ipk/po/zh-cn 2>/dev/null
+	ln -s zh-cn $pkg/po/zh_Hans 2>/dev/null
+	ln -s zh_Hans $pkg/po/zh-cn 2>/dev/null
 done
