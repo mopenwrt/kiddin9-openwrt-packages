fa0a00ae3b7bf2130b973c08b645d1382066ff88
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-09-02 02:26:27 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for e in $(ls -d luci-*/po); do
-	if [[ -f $e/zh-cn && ! -f $e/zh_Hans ]]; then
+	if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
 		ln -s zh-cn $e/zh_Hans 2>/dev/null
-	elif [[ -f $e/zh_Hans && ! -f $e/zh-cn ]]; then
+	elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
 		ln -s zh_Hans $e/zh-cn 2>/dev/null
 	fi
 done
