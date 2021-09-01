7cce0b62a1acc34677471caa05bd0f7bb9d478d9
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-09-02 02:01:50 +0800

    Update convert_translation.sh

--- diy/convert_translation.sh
+++ diy/convert_translation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for pkg in $(ls -d luci-*); do
-	ln -s zh-cn $pkg/po/zh_Hans 2>/dev/null
-	ln -s zh_Hans $pkg/po/zh-cn 2>/dev/null
+for e in $(ls -d luci-*/po); do
+	if [ -f $e/zh-cn && ! f $e/zh_Hans ]; then
+		ln -s zh-cn $e/zh_Hans 2>/dev/null
+	elif [ -f $e/zh_Hans && ! f $e/zh-cn ]; then
+		ln -s zh_Hans $e/zh-cn 2>/dev/null
+	fi
 done
